fba96d436e537bb1e7a0cadb3dd6e9432d8dffb6
smash-96 <maisamshah57@gmail.com>
2022-09-26 15:25:59 +0500

    updates

--- components/FundamentalBox/FundamentalBox.tsx
+++ components/FundamentalBox/FundamentalBox.tsx
@@ -17,7 +17,7 @@ export const FundamentalBox = ({ boxData, group }: BoxProps) => {
         <div className="FundamentalBox flex items-center flex-wrap gap-[12px]">
             {
                 boxData.map((Item: any, index: number) =>
-                    <div key={index} className="box-otr">
+                    <div key={index} className="box-otr w-[69px]">
                         <p className="question-text heading-XS text-primary-dark text-center mb-[4px] bg-transparent">
                             {Item.question_text}
                         </p>
@@ -26,7 +26,7 @@ export const FundamentalBox = ({ boxData, group }: BoxProps) => {
                         {/* red-arrow-box-text */}
                         {/* red-arrow-box */}
                         {/* ${Item.question_text === 'Q4.19' ? `red-text-box` : `` || Item.question_text === 'Q1.20' ? `red-text-box` : `` || Item.question_text === 'Q1.21' ? `green-background-box` : `` || Item.question_text === 'Q2.21' ? `green-background-box red-text-box` : ``} */}
-                        <div className={`box-inr ${(group === "Fundamentals Snapshot" || group === "Sales Growth") && (Item.question_text === 'Q4.19' || Item.question_text === 'Q1.20' || Item.question_text === 'Q2.21') && `red-text-box`} ${group === "Return on Equity" && ((Item.question_text === "Q2.19" || Item.question_text === "Q4.19") ? `red-arrow-box`: `green-arrow-box`)} ${group === "Institutionnal Ownership" && ((Item.question_text === "Q1.20" || Item.question_text === "Q4.19") ? `red-arrow-box-text`: `green-arrow-box-text`)} py-[12px] px-[9px] bg-grey-shade1 rounded-8`}>
+                        <div className={`box-inr ${(group === "Fundamentals Snapshot" || group === "Sales Growth") && (Item.question_text === 'Q4.19' || Item.question_text === 'Q1.20' || Item.question_text === 'Q2.21') && `red-text-box`} ${group === "Return on Equity" && ((Item.question_text === "Q2.19" || Item.question_text === "Q4.19") ? `red-arrow-box` : `green-arrow-box`)} ${group === "Institutionnal Ownership" && ((Item.question_text === "Q1.20" || Item.question_text === "Q4.19") ? `red-arrow-box-text` : `green-arrow-box-text`)} py-[12px] px-[9px] bg-grey-shade1 rounded-8`}>
                             <div className="arrow-red flex items-center justify-center mb-[6px]">
                                 <Image className="object-contain" width="16px" height="16px" src="/svg/arrow-red.svg" alt="arrow" />
                             </div>
