060315adccc0b35612a914198fc6886238bee0bf
Edouard Soutzo <edouard.soutzo.mc@gmail.com>
2022-09-29 07:51:38 +0000

    update margin

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -28,7 +28,7 @@ export const Header = () => {
                         </p>
                     </li>
                     <li className="menu-li">
-                        <a href="" className="menu-a heading-S text-primary-dark md:ml-20px">
+                        <a href="" className="menu-a heading-S text-primary-dark md:ml-5">
                             Watchlist
                         </a>
                     </li>
