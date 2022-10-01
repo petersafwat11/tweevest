908f010450fd02a61abfb6b515346ee107594bd8
smash-96 <maisamshah57@gmail.com>
2022-10-01 21:32:44 +0500

    misc

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -63,8 +63,8 @@ export const Header = () => {
                                     <div className="absolute top-0 left-[-44px] flex pt-4 pr-2 sm:pr-4 xs:hidden" onClick={handleCloseMenu}>
                                         <button type="button" className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
                                             <span className="sr-only">Close panel</span>
-                                            <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                            <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                             </svg>
                                         </button>
                                     </div>
@@ -74,8 +74,8 @@ export const Header = () => {
                                                 <Image className="object-contain object-left" width="140px" height="38px" src="/brand-logo.png" alt="logo" />
                                             </a>
                                             <button type="button" className="rounded-md hidden xs:block text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white" onClick={handleCloseMenu}>
-                                                <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                                <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                                 </svg>
                                             </button>
                                         </div>
