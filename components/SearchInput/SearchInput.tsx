02df466dfe211e1e60718a1e2b0b7e36208b0b42
smash-96 <maisamshah57@gmail.com>
2022-10-15 14:30:02 +0500

    redux added

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -1,11 +1,63 @@
 import React, { FC } from "react";
 import Image from 'next/image';
-
+import { api } from "../../utils";
+import axios from "axios"
+import { useDispatch } from "react-redux"
+import { setLoader,setAPI1Data, setAPI2Data, setAPI3Data, setAPI4Data, setAPI5Data, setAPI6Data, setAPI7Data, setAPI8Data, setAPI9Data } from "../../store/stockSlice"
 interface SearchProps {
     handleChange: any,
     filteredData: any,
 }
+
 export const SearchInput: FC<SearchProps> = ({ handleChange, filteredData }) => {
+    const dispatch = useDispatch()
+    const callAPIs = async (symbol: string) => {
+        dispatch(setLoader(true))
+        console.log("HERE1")
+        let one = `/stocks/overview/${symbol}`
+        let two = `/stocks/live-quote/${symbol}`
+        let three = `/stocks/shares-float/${symbol}`
+        let four = `/stocks/company-profile/${symbol}`
+        let five = `/stocks/peers/${symbol}`
+        let six = `/stocks/price-change/${symbol}`
+        let seven = `/stocks/institutional-ownership/${symbol}`
+        let eight = `/stocks/fundamentals-snapshot/${symbol}`
+        let nine = `/stocks/fundamentals-snapshot-vs-peers/${symbol}`
+        const requestOne = api.get(one);
+        const requestTwo = api.get(two);
+        const requestThree = api.get(three);
+        const requestFour = api.get(four);
+        const requestFive = api.get(five);
+        const requestSix = api.get(six);
+        const requestSeven = api.get(seven);
+        const requestEight = api.get(eight);
+        const requestNine = api.get(nine);
+        await axios.all([requestOne, requestTwo, requestThree, requestFour, requestFive, requestSix, requestSeven, requestEight, requestNine]).then(axios.spread((...responses) => {
+            const responseOne = responses[0]
+            const responseTwo = responses[1]
+            const responseThree = responses[2]
+            const responseFour = responses[3]
+            const responseFive = responses[4]
+            const responseSix = responses[5]
+            const responseSeven = responses[6]
+            const responseEight = responses[7]
+            const responseNine = responses[8]
+            dispatch(setAPI1Data(responseOne.data))
+            dispatch(setAPI2Data(responseTwo.data))
+            dispatch(setAPI3Data(responseThree.data))
+            dispatch(setAPI4Data(responseFour.data))
+            dispatch(setAPI5Data(responseFive.data))
+            dispatch(setAPI6Data(responseSix.data))
+            dispatch(setAPI7Data(responseSeven.data))
+            dispatch(setAPI8Data(responseEight.data))
+            dispatch(setAPI9Data(responseNine.data))
+            console.log("HERE2", responses)
+            dispatch(setLoader(false))
+        })).catch(errors => {
+            // react on errors.
+            console.log("HERE3", errors)
+        })
+    }
     return (
         <>
             <form className="w-[100%] flex">
@@ -19,7 +71,7 @@ export const SearchInput: FC<SearchProps> = ({ handleChange, filteredData }) => 
                     <ul className="drop-down-ul w-[100%] bg-white border-[1px] border-border-shade1 rounded-8 overflow-hidden">
                         {filteredData?.data?.map((data: any, index: number) => {
                             return (
-                                <li key={index} className="drop-down-li text-left w-[100%] flex border-b-[1px] border-b-border-shade1">
+                                <li onClick={() => callAPIs(data.symbol)} key={index} className="drop-down-li text-left w-[100%] flex border-b-[1px] border-b-border-shade1">
                                     <a href="#" className="drop-down-a heading-S p-[16px] text-primary-dark w-[100%] bg-white flex items-center gap-[24px] hover:bg-primary-default hover:text-white">
                                         <span className="heading-SB text-primary-dark">{data.symbol}</span> <span className="heading-SB text-primary-dark2 flex-1">{data.name}</span>
                                     </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,25 @@ import { Cta } from '../components';
 import { TabsComponent } from '../components';
 import { Footer } from '../components';
 import styles from '../styles/Home.module.css';
-
+import { useSelector } from 'react-redux';
+import { selectLoader,selectAPI1Data,selectAPI2Data,selectAPI3Data,selectAPI4Data,selectAPI5Data,selectAPI6Data,selectAPI7Data,selectAPI8Data,selectAPI9Data } from '../store/stockSlice';
 const Home: NextPage = () => {
+  const API1Data = useSelector(selectAPI1Data)
+  const API2Data = useSelector(selectAPI2Data)
+  const API3Data = useSelector(selectAPI3Data)
+  const API4Data = useSelector(selectAPI4Data)
+  const API5Data = useSelector(selectAPI5Data)
+  const API6Data = useSelector(selectAPI6Data)
+  const API7Data = useSelector(selectAPI7Data)
+  const API8Data = useSelector(selectAPI8Data)
+  const API9Data = useSelector(selectAPI9Data)
+  const loader = useSelector(selectLoader)
+  
+  let temp = [API1Data,API2Data,API3Data,API4Data,API5Data,API6Data,API7Data,API8Data,API9Data,]
+  console.log("DATA", temp)
+  if (loader){
+    return null // replace with some loading overlay later
+  }
   return (
     <div className={styles.container}>
       <Header />
