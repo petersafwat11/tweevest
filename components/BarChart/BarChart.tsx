6caf83a3ae380246699d16adc81e0253bc92a8af
jamalbutt1232 <jamalbutt1232@gmail.com>
2022-10-18 16:51:08 +0500

    sslider component update

--- components/BarChart/BarChart.tsx
+++ components/BarChart/BarChart.tsx
@@ -1,120 +1,178 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 import { BartextChart } from "..";
 import { BartextChart2 } from "..";
-import { Dropdown } from 'antd';
+import { Dropdown } from "antd";
+import { useSelector } from "react-redux";
+import { selectAPI8Data, selectAPI9Data } from "../../store/stockSlice";
 
 const links = [
-    { title: 'Price to sales', link: '' },
-    { title: 'Price to sales', link: '' },
-    { title: 'Price to sales', link: '' },
-    { title: 'Price to sales', link: '' },
+  { title: "Price to sales", link: "" },
+  { title: "Price to sales", link: "" },
+  { title: "Price to sales", link: "" },
+  { title: "Price to sales", link: "" },
 ];
 
 export const BarChart = () => {
-    return (
-        <>
-            <div className="BarChart-box-otr">
-                <div className="BarChart-box-inr px-[32px] pt-[32px] pb-[40px]">
-                    <div className="chart-heading-otr">
-                        <p className="heading-SB text-primary-dark flex items-center">
-                            Fundamentals Snapshot
-                            <span className="ml-[8px] flex">
-                                <Image className="object-contain" width="16px" height="16px" src="/svg/info-circle.svg" alt="Shape" />
-                            </span>
-                        </p>
-                        <div className="static-boxes-otr flex items-center flex-wrap gap-[7px] mt-[10px] mb-[40px]">
-                            <div className="static-box w-[31%] 2xl:w-[48%]">
-                                <h3 className="heading-h3 text-primary-dark">
-                                    45x
-                                </h3>
-                                <p className="heading-XS text-primary-dark2">
-                                    P/E Ratio
-                                </p>
-                            </div>
-                            <div className="static-box w-[31%] 2xl:w-[48%]">
-                                <h3 className="heading-h3 text-primary-dark">
-                                    25x
-                                </h3>
-                                <p className="heading-XS text-primary-dark2">
-                                    P/S Ratio
-                                </p>
-                            </div>
-                            <div className="static-box w-[31%] 2xl:w-[48%]">
-                                <h3 className="heading-h3 text-primary-dark">
-                                    4x
-                                </h3>
-                                <p className="heading-XS text-primary-dark2">
-                                    P/Book Ratio
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="earning-revenue-otr mb-[24px] flex items-center gap-[16px]">
-                        <div className="content-otr">
-                            <p className="heading-XS text-primary-dark2 mb-[26px]">
-                                Earnings
-                            </p>
-                            <p className="heading-XS text-primary-dark2">
-                                Sales
-                            </p>
-                        </div>
-                        <div className="chart-otr relative z-10 pt-[20px] pb-[1px] border-l-[1px] border-primary-dark2 w-[100%]">
-                            <BartextChart2 type={1} value={"80"} text={"380"} />
-                            <BartextChart2 type={2} value={"100"} text={"30B"} />
-                        </div>
-                    </div>
-                    <p className="heading-MB text-primary-default mb-[40px]">
-                        How AAPL compre to sector?
-                    </p>
-                    <div className="drop-chart-otr relative overflow-hidden">
-                        <div className="drop-inr flex items-center justify-end">
-                            <Dropdown
-                                placement="bottomRight"
-                                overlay={
-                                    <div>
-                                        {links?.map(({ title, link }) => (
-                                            <div
-                                                className="bg-white first:rounded-t-[8px] last:rounded-b-[8px] min-w-[204px] min-h-[45px] flex items-center shadow-Shadow1 hover:bg-primary-default hover:text-white transition-all cursor-pointer border-[1px]"
-                                                key={title}
-                                            >
-                                                <span className="ml-[16px]">{title}</span>
-                                            </div>
-                                        ))}
-                                    </div>
-                                }
-                                trigger={['click']}
-                            >
-                                <div className="cursor-pointer flex items-center gap-[8px] pt-[6px] pr-[8px] pb-[8px] pl-[8px] border-[1px] border-border-shade1 rounded-8">
-                                    <p className="heading-XS text-primary-dark2">
-                                        Price to sales
-                                    </p>
-                                    <Image className="object-contain" src="/svg/arrow-down.svg" alt="menu" width="20px" height="20px" />
-                                </div>
-                            </Dropdown>
-                        </div>
+  const API8Data = useSelector(selectAPI8Data);
+  const API9Data = useSelector(selectAPI9Data);
 
-                        <div className="content-otr text-center mr-[40px] mb-[16px]">
-                            <p className="heading-MB text-primary-dark">
-                                APPL
-                            </p>
-                            <p className="heading-MB text-primary-dark2">
-                                25x
-                            </p>
-                        </div>
-                        <div className="dropdown-chart-otr relative pt-[8px] pb-[8px] border-l-[1px] border-primary-dark2">
-                            <div className="line-dash absolute h-[100%]"></div>
-                            <div className="drop-chart-inr relative z-10">
-                                <BartextChart value={"50"} text={"Meta 34x"} color={"red"} />
-                                <BartextChart value={"70"} text={"Meta 34x"} color={"secondary-blue"} />
-                                <BartextChart value={"60"} text={"Meta 34x"} color={"tertiary-yellow"} />
-                                <BartextChart value={"55"} text={"Meta 34x"} color={"state-success"} />
-                                <BartextChart value={"80"} text={"Meta 34x"} color={"primary-blue"} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  console.log("API8Data: ", API8Data);
+  console.log("API9Data: ", API9Data);
+  return (
+    <>
+      <div className="BarChart-box-otr">
+        <div className="BarChart-box-inr px-[32px] pt-[32px] pb-[40px]">
+          <div className="chart-heading-otr">
+            <p className="heading-SB text-primary-dark flex items-center">
+              Earnings Growth
+              <span className="ml-[8px] flex">
+                <Image
+                  className="object-contain"
+                  width="16px"
+                  height="16px"
+                  src="/svg/info-circle.svg"
+                  alt="Shape"
+                />
+              </span>
+            </p>
+            <div className="static-boxes-otr flex items-center flex-wrap gap-[7px] mt-[10px] mb-[40px]">
+              <div className="static-box w-[31%] 2xl:w-[48%]">
+                <h3 className="heading-h3 text-primary-dark">
+                  {Number(API8Data.pe).toFixed(2)}x
+                </h3>
+                <p className="heading-XS text-primary-dark2">P/E Ratio</p>
+              </div>
+              <div className="static-box w-[31%] 2xl:w-[48%]">
+                <h3 className="heading-h3 text-primary-dark">
+                  {" "}
+                  {Number(API8Data.pe).toFixed(2)}x
+                </h3>
+                <p className="heading-XS text-primary-dark2">P/S Ratio</p>
+              </div>
+              <div className="static-box w-[31%] 2xl:w-[48%]">
+                <h3 className="heading-h3 text-primary-dark">
+                  {Number(API8Data.priceToBook).toFixed(2)}x
+                </h3>
+                <p className="heading-XS text-primary-dark2">P/Book Ratio</p>
+              </div>
             </div>
-        </>
-    )
-}
+          </div>
+          <div className="earning-revenue-otr mb-[24px] flex items-center gap-[16px]">
+            <div className="content-otr">
+              <p className="heading-XS text-primary-dark2 mb-[26px]">
+                Earnings
+              </p>
+              <p className="heading-XS text-primary-dark2">Sales</p>
+            </div>
+            <div className="chart-otr relative z-10 pt-[20px] pb-[1px] border-l-[1px] border-primary-dark2 w-[100%]">
+              <BartextChart2 type={1} value={"80"} text={"380"} />
+              <BartextChart2 type={2} value={"100"} text={"30B"} />
+            </div>
+          </div>
+          <p className="heading-MB text-primary-default mb-[40px]">
+            How AAPL compre to sector?
+          </p>
+          <div className="drop-chart-otr relative overflow-hidden">
+            <div className="drop-inr flex items-center justify-end">
+              <Dropdown
+                placement="bottomRight"
+                overlay={
+                  <div>
+                    {links?.map(({ title, link }) => (
+                      <div
+                        className="bg-white first:rounded-t-[8px] last:rounded-b-[8px] min-w-[204px] min-h-[45px] flex items-center shadow-Shadow1 hover:bg-primary-default hover:text-white transition-all cursor-pointer border-[1px]"
+                        key={title}
+                      >
+                        <span className="ml-[16px]">{title}</span>
+                      </div>
+                    ))}
+                  </div>
+                }
+                trigger={["click"]}
+              >
+                <div className="cursor-pointer flex items-center gap-[8px] pt-[6px] pr-[8px] pb-[8px] pl-[8px] border-[1px] border-border-shade1 rounded-8">
+                  <p className="heading-XS text-primary-dark2">
+                    Price to sales
+                  </p>
+                  <Image
+                    className="object-contain"
+                    src="/svg/arrow-down.svg"
+                    alt="menu"
+                    width="20px"
+                    height="20px"
+                  />
+                </div>
+              </Dropdown>
+            </div>
+
+            <div className="content-otr text-center mr-[40px] mb-[16px]">
+              <p className="heading-MB text-primary-dark">APPL</p>
+              <p className="heading-MB text-primary-dark2">25x</p>
+            </div>
+            <div className="dropdown-chart-otr relative pt-[8px] pb-[8px] border-l-[1px] border-primary-dark2">
+              <div className="line-dash absolute h-[100%]"></div>
+              <div className="drop-chart-inr relative z-10">
+                <BartextChart
+                  value={"50"}
+                  text={Number(API9Data[0]?.priceToSales).toFixed(2)}
+                  color={"red"}
+                />
+                <BartextChart
+                  value={"70"}
+                  text={Number(API9Data[1]?.priceToSales).toFixed(2)}
+                  color={"secondary-blue"}
+                />
+                <BartextChart
+                  value={"60"}
+                  text={Number(API9Data[2]?.priceToSales).toFixed(2)}
+                  color={"tertiary-yellow"}
+                />
+                <BartextChart
+                  value={"55"}
+                  text={Number(API9Data[3]?.priceToSales).toFixed(2)}
+                  color={"state-success"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[4]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[5]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[6]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[7]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[8]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[9]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+                <BartextChart
+                  value={"80"}
+                  text={Number(API9Data[10]?.priceToSales).toFixed(2)}
+                  color={"primary-blue"}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL:"https://staging.api.tweevest.com",
+    NEXT_PUBLIC_API_URL: "https://staging.api.tweevest.com",
     USERNAME: "tweevest-nextjs-server",
-    PASSWORD: "vbpx8divic#caq*28y354c&wjhhzvx6t"
+    PASSWORD: "vbpx8divic#caq*28y354c&wjhhzvx6t",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
