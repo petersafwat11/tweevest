2263d31ac4ba9af6ece701332725fb99f635fc66
jamalbutt1232 <jamalbutt1232@gmail.com>
2022-10-26 22:25:50 +0000

    Add new file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_URL: "https://staging.api.tweevest.com",
+    USERNAME: "tweevest-nextjs-server",
+    PASSWORD: "vbpx8divic#caq*28y354c&wjhhzvx6t",
+  },
+};
+
+module.exports = nextConfig;
