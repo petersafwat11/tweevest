7384d81c4a0b69e34a5f2408d403efc09b4c2e77
jamalbutt1232 <jamalbutt1232@gmail.com>
2022-11-11 03:06:05 +0500

    changes

--- components/BarChart/BarChart.tsx
+++ components/BarChart/BarChart.tsx
@@ -20,11 +20,11 @@ export const BarChart = () => {
       ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + "B"
       : // Six Zeroes for Millions
       Math.abs(Number(labelValue)) >= 1.0e6
-        ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + "M"
-        : // Three Zeroes for Thousands
-        Math.abs(Number(labelValue)) >= 1.0e3
-          ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + "K"
-          : Math.abs(Number(labelValue));
+      ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + "M"
+      : // Three Zeroes for Thousands
+      Math.abs(Number(labelValue)) >= 1.0e3
+      ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + "K"
+      : Math.abs(Number(labelValue));
   }
 
   const [selectedValue, setSelectedValue] = useState("pe");
